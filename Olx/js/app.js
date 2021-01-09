f87d47efd23f56839537feefd83ee7e4d8384bf2
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-01-09 12:01:21 +0500

    add image rendring function

--- Olx/js/app.js
+++ Olx/js/app.js
@@ -1,13 +1,9 @@
 let database = firebase.database().ref("sellIteam");
-let key;
 var container_1 = document.getElementById("container_1");
-var uploadTask;
+var files = [];
 
-async function myImg() {
-   var files = [];
+function myImg() {
    var reader;
-
-   key = database.push().key;
 
    var input = document.createElement("input");
    input.type = "file";
@@ -22,69 +18,69 @@ async function myImg() {
       reader.readAsDataURL(files[0]);
    };
    input.click();
-
-   uploadTask = firebase
-      .storage()
-      .ref()
-      .child("images/" + "imageName" + ".jpeg")
-      .put(files[0]);
-
-   // uploadTask.snapshot.ref.getDownloadURL().then(function (downlaodURL) {
-   //    database.child(key).set({
-   //       ImageUrl_1: downlaodURL,
-   //    });
-   // });
 }
 
-// function Submit() {
-//    let inpPrice = document.getElementById("inpPrice").value;
-//    let inpDesc = document.getElementById("inpDesc").value;
-//    // let inpCity = document.getElementById("inpCity").value;
+function Submit() {
+   var key = database.push().key;
+   let inpPrice = document.getElementById("inpPrice").value;
+   let inpDesc = document.getElementById("inpDesc").value;
+   // let inpCity = document.getElementById("inpCity").value;
 
-//    database.child(key).update({
-//       inpPrice_1: inpPrice,
-//       inpDesc_1: inpDesc,
-//       // inpCity_1: inpCity,
-//       key,
-//    });
-// }
-// database.on("child_added", function (data) {
-//    let box = document.createElement("div");
-//    box.setAttribute("class", "card col");
-//    container_1.appendChild(box);
+   var uploadTask = firebase
+      .storage()
+      .ref("images1/" + "imageName" + key + ".png")
+      .put(files[0]);
 
-//    let img = document.createElement("img");
-//    img.setAttribute("src", data.val().ImageUrl_1);
-//    img.setAttribute("class", "card-img-top");
+   uploadTask.on("state_changed", function () {
+      uploadTask.snapshot.ref.getDownloadURL().then(function (downlaodURL) {
+         database.child(key).set({
+            inpPrice_1: inpPrice,
+            inpDesc_1: inpDesc,
+            // inpCity_1: inpCity,
+            key,
+            ImageUrl_1: downlaodURL,
+         });
+      });
+   });
+}
 
-//    let div_1 = document.createElement("div");
-//    div_1.setAttribute("class", "card-body");
+database.on("child_added", function (data) {
+   let box = document.createElement("div");
+   box.setAttribute("class", "card col");
+   container_1.appendChild(box);
 
-//    let Rs = document.createElement("h4");
-//    Rs.setAttribute("class", "Rs card-title");
+   let img = document.createElement("img");
+   img.setAttribute("src", data.val().ImageUrl_1);
+   img.setAttribute("class", "card-img-top");
 
-//    let rsTextNode = document.createTextNode(data.val().inpPrice_1);
-//    Rs.appendChild(rsTextNode);
+   let div_1 = document.createElement("div");
+   div_1.setAttribute("class", "card-body");
 
-//    let desc = document.createElement("p");
-//    desc.setAttribute("class", "card-text");
+   let Rs = document.createElement("h4");
+   Rs.setAttribute("class", "Rs card-title");
 
-//    let descTextNode = document.createTextNode(data.val().inpDesc_1);
-//    desc.append(descTextNode);
+   let rsTextNode = document.createTextNode(data.val().inpPrice_1);
+   Rs.appendChild(rsTextNode);
 
-//    div_1.appendChild(Rs);
-//    div_1.appendChild(desc);
+   let desc = document.createElement("p");
+   desc.setAttribute("class", "card-text");
 
-//    let div_2 = document.createElement("div");
-//    div_2.setAttribute("class", "card-footer");
+   let descTextNode = document.createTextNode(data.val().inpDesc_1);
+   desc.append(descTextNode);
 
-//    // let city = document.createElement("small");
-//    // let inpCity = document.createTextNode(data.val().inpCity_1);
-//    // city.appendChild(inpCity);
+   div_1.appendChild(Rs);
+   div_1.appendChild(desc);
 
-//    // div_2.appendChild(city);
+   let div_2 = document.createElement("div");
+   div_2.setAttribute("class", "card-footer");
 
-//    box.appendChild(img);
-//    box.appendChild(div_1);
-//    box.appendChild(div_2);
-// });
+   // let city = document.createElement("small");
+   // let inpCity = document.createTextNode(data.val().inpCity_1);
+   // city.appendChild(inpCity);
+
+   // div_2.appendChild(city);
+
+   box.appendChild(img);
+   box.appendChild(div_1);
+   box.appendChild(div_2);
+});
